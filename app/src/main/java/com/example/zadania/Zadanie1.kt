167808f42175b8cc39322a831ff95c55af92b8f4
kornel <kornel@mail.com>
2024-12-09 11:28:52 +0100

    nic nie dziala

--- app/src/main/java/com/example/zadania/Zadanie1.kt
+++ app/src/main/java/com/example/zadania/Zadanie1.kt
@@ -1,2 +1,20 @@
-package com.example.zadania
+//interface RollingTool{
+//    fun roll(){
+//        println("Rzut został wykonany")
+//    }
+//}
+//
+//class Coin : RollingTool{
+//    override fun roll() {
+//        println()
+//    }
+//}
+
+fun main(){
+    val a = Math.random()*10
+    fun r(){
+        println(a)
+    }
+}
+
 
